d87b4416fd5563149ee8b1749bb621c4d8fa594b
Brendan Woo <woo.brendan@gmail.com>
2022-02-01 21:48:11 +0000

    added new wection area to index.html. added js script file for jquery to be used on index. this script gets the counter and adjusts based on inputs going into the textarea

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('#tweet-text').on('input', function() {
+
+    // console.log("this.parent is: ", this.parentNode.childNodes[5].childNodes[3].innerHTML)
+    
+    let messageVal = $(this).val().length
+
+    //input tweet is over 140 char
+    if (Number(messageVal) > 140 ) {
+      $('.counter').css('color', 'red');
+      $('.counter').html(140 - messageVal);
+    } else {
+      $('.counter').css('color', 'inherit');
+      $('.counter').html(140 - messageVal);
+    }
+  })
+});
+
