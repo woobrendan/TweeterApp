7ccaae1334a94ae482c8ac028cedb393cec8d8a0
Brendan Woo <woo.brendan@gmail.com>
2022-02-02 19:28:38 +0000

    Changed css styling to have avatar background as white. removed tweet-container from html and put into function createTweet within the client.js file. implement the function to create a tweet and created another function to append the newly generated tweets from a hardcoded database (temporary)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,73 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1643571093563
+}
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+const createTweetElement = (tweetData) => {
+  const layout = `
+  <section class="tweet-container">
+    <header class="tweet-header">
+      <div>
+        <img src="${tweetData.user.avatars}">
+            <h3>${tweetData.user.name}</h3>
+      </div>
+      <h4>${tweetData.user.handle}</h4>
+    </header>
+    <article class="tweet">
+      <p>${tweetData.content.text}</p>
+    </article>
+    <footer>
+      <h6>${timeago.format(tweetData.created_at)}</h6>
+      <div>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </section>`;
+      
+return layout;
+      
+};
+
+const renderTweets = function(tweetArr) {
+  for (let tweet of tweetArr) {
+    console.log(tweet)
+    const indivTweet = createTweetElement(tweet)
+    $('.container').append(indivTweet)
+  }
+}
