21ad61031836fc0293704a2b6fe1ab4a80764f2f
Brendan Woo <woo.brendan@gmail.com>
2022-02-02 20:39:56 +0000

    changed css sheets to change background color to blue. added ajax request to client.js to grab the JSON  from tweets URL and render them into tweets on page.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,30 +16,6 @@ const tweetData = {
   "created_at": 1643571093563
 }
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 const createTweetElement = (tweetData) => {
   const layout = `
   <section class="tweet-container">
@@ -87,4 +63,15 @@ $(() => {
   
     })
   })
+
+  const loadTweets = function() {
+    $.ajax({
+      url: 'http://localhost:8080/tweets',
+      method: 'GET',
+      dataType: 'JSON'
+    }).then((arrOfTweets) => {
+      renderTweets(arrOfTweets);
+    })
+  }
+  loadTweets();
 });
